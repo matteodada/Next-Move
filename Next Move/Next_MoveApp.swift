9baaca3bb529d9236279cba78212a24a9d0b5a71
Matteo Dada <matteo.onillon.apple@gmail.com>
2021-03-29 11:18:06 +0200

    Initial Commit

---
+++ Next Move/Next_MoveApp.swift
@@ -0,0 +1,17 @@
+//
+//  Next_MoveApp.swift
+//  Next Move
+//
+//  Created by Matteo Dada Onillon on 3/29/21.
+//
+
+import SwiftUI
+
+@main
+struct Next_MoveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
